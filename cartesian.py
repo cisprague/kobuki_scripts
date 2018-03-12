de8e561a61c5b07490a9346d55382297ee3d4f8b
cisprague <christopher.iliffe.sprague@gmail.com>
2018-03-12 11:02:47 +0100

    Scripts

--- cartesian.py
+++ cartesian.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python
 
 import rospy, sys
+import numpy as np
 from ras_lab1_msgs.msg import PWM
+from ras_lab1_msgs.msg import Encoders
 from geometry_msgs.msg import Twist
 
-def callback(data):
-    print(data)
+class cartesian(object):
 
-def cartesian(v, w):
+    def __init__(self, v, w):
 
-    # publisher
-    pub = rospy.Publisher('/kobuki/pwm', PWM)
+        # desired linear and angular velocity
+        self.v, self.w = v, w
 
-    # subscriber
-    rospy.Subscriber('/mobile_base/commands/velocity', Twist, callback)
+        # publisher
+        pub = rospy.Publisher('kobuki/pwm', PWM)
 
-    # node
-    rospy.init_node('cartesian')
+        # subscriber
+        rospy.Subscriber('/kobuki/encoders', Encoders, self.callback)
 
-    rospy.spin()
+        # initialise node
+        rospy.init_node('cartesian')
+
+        rospy.spin()
+
+
+    def callback(self, data):
+
+        # left and right wheel signals
+        de1, de2 = data.delta_encoder1, data.delta_encoder2
+
+        # error
+        ew1, ew2 = de1*2*np.pi*10/360, de2*2*np.pi*10/360
+
+        print(ew1)
+
 
 if __name__ == '__main__':
     cartesian(5, 5)
